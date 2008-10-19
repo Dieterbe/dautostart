08e3e6bf3da7da10ffd57ceb550c1569f95c94d9
Dieter Plaetinck <dieter@plaetinck.be>
2008-10-19 12:52:15 +0200

    fixed a lot of bugs. almost works now

--- dautostart.sh
+++ dautostart.sh
@@ -9,79 +9,88 @@ echo " ** dautostart starting..."
 
 # This functions maintains the list of desktop files to be used.  It adds absolute paths to the list and removes 'overruled' items.
 updatelist () {
-	echo "updatelist called with arg $1"
+	#echo "**** updatelist called with arg $1"
 	dir=`echo "$1" | sed 's#/$##'`
 	new_list=`ls -1 $1/autostart/*.desktop 2>/dev/null`
-	echo "** newlist: $new_list"
-	echo "** oldlist: $old_list"
 	# copy full_list to old_list. go over new items and add them at the end. remove old item if it becomes invalid.
 	while read new
 	do
 		if test "x$new" != "x"
 		then
-		old_list=$full_list
-		full_list=
-		new_base=`basename "$new"`
-		found=no
-		if test "x$old_list" != "x"
-		then
-			while read old
-			do
-				old_base=`basename "$old"`
-				if test "x$new_base" != "x" -a "x$old_base" != "x"
-				then
-					echo "** comparing $new_base --- $old_base"
-					if test "$new_base" = "$old_base"
+			echo "Checking for inclusion $new ..."
+			new_base=`basename "$new"`
+			found=no
+			if test "x$full_list" != "x"
+			then
+				old_list=$full_list
+				full_list=
+				while read old
+				do
+					old_base=`basename "$old"`
+					if test "x$new_base" != "x" -a "x$old_base" != "x"
 					then
-						found=yes
-					else
-						full_list="$full_list
+						if test "$new_base" != "$old_base"
+						then
+							if test "x$full_list" = "x"
+							then
+								full_list="$old"
+							else
+								full_list="$full_list
 $old"
+							fi
+						fi
 					fi
-				fi
-			done <<< "$old_list"
-		fi
-		if test "$found" = no -a "x$new" != "x"
-		then
-			full_list="$full_list
+				done <<< "$old_list"
+			fi
+		
+			if test "x$new" != "x"
+			then
+				if test "x$full_list" = "x"
+				then
+					full_list="$new"
+				else
+					full_list="$full_list
 $new"
-		fi
+				fi
+			fi
 		fi
 	done <<< "$new_list"
-	echo "** FULList: $full_list"
 }
 
-# This function processes one entry of the list and does whatever needs to be done.
-process_entry () {
-for file in "$full_list"
+# This function processes all entries of the list and does whatever needs to be done.
+process_entries () {
+while read file
 do
-	if grep -q -E "^Hidden=true" "$i"
+	echo "Processing $file ..."
+	if grep -q -E "^Hidden=true" "$file"
 	then
 		break
 	fi
-	if test "x$DESKTOP_ENVIRONMENT" ! "x" #if we don't know which DE this is, we can't use the variables OnlyShowIn and NotShowIn
+	if test "x$DESKTOP_ENVIRONMENT" != "x" #if we don't know which DE this is, we can't use the variables OnlyShowIn and NotShowIn
 	then
 		# Note that the autostart specs mentions "a list of strings identifying the DE's". We cannot be 100% accurate.
-		if grep -Eq "^OnlyShowIn=" "$1"
+		if grep -Eq "^OnlyShowIn=" "$file"
 		then
-			if grep -E "^OnlyShowIn=" "$1" | grep -qv "$DESKTOP_ENVIRONMENT"
+			if grep -E "^OnlyShowIn=" "$file" | grep -qv "$DESKTOP_ENVIRONMENT"
 			then
 				break
 			fi
 		fi
-		if grep -E "^NotShowIn=" "$i" | grep -q "$DESKTOP_ENVIRONMENT"
+		if grep -E "^NotShowIn=" "$file" | grep -q "$DESKTOP_ENVIRONMENT"
 		then
 			break
 		fi
 	fi
 	
-	try_exec=`grep -E "^TryExec=" "$i" | cut -d'=' -f2`
+	try_exec=`grep -E "^TryExec=" "$file" | cut -d'=' -f2`
 	if test "$try_exec" && test -x "$try_exec"
 	then
+		which "$try_exec"
 		echo "executing $try_exec ...."
 		#$try_exec &
 	else
-		exec_cmd=`grep -E "^Exec=" "$i" | cut -d'=' -f2`
+		# Exec key is not documented in spec but all my .desktop files have this, so ...
+		exec_cmd=`grep -E "^Exec=" "$file" | cut -d'=' -f2`
 		if test "$exec_cmd" && test -x "$exec_cmd"
 		then
 			echo "executing $exec_cmd ...."
@@ -89,7 +98,7 @@ do
 		fi
 	fi
 
-done
+done <<< "$full_list"
 }
 
 
@@ -107,5 +116,6 @@ do
 	updatelist "$dir"
 done
 
+process_entries
 
 echo " ** dautostart done..."
